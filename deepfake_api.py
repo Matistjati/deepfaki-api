e3c8e1c4f215e0ec63dc45fb44dc0659d14cb970
Jaushau <matistjati@outlook.com>
2020-08-20 23:35:02 +0200

    Turned the code into an api format

--- deepfake_api.py
+++ deepfake_api.py
@@ -11,28 +11,50 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-if not os.path.isfile('checkpoints/vox-cpk.pth.tar'):
-    raise FileNotFoundError("Could not find training data. Check checkpoints folder")
+class DeepFakeApi:
+    @staticmethod
+    def __init__():
+        DeepFakeApi.initialized = True
 
-source_image = imageio.imread("C:/Users/Matis/Desktop/deepfake api/input/obama.jpg")
-reader = imageio.get_reader("C:/Users/Matis/Desktop/deepfake api/input/baka mitai driver.mp4")
+        if not os.path.isfile('checkpoints/vox-cpk.pth.tar'):
+            raise FileNotFoundError("Could not find training data. Check checkpoints folder")
 
-fps = reader.get_meta_data()['fps']
-driving_video = []
-try:
-    for im in reader:
-        driving_video.append(im)
-except RuntimeError:
-    pass
-reader.close()
+        print("Loading checkpoints")
+        DeepFakeApi.generator, DeepFakeApi.kp_detector = load_checkpoints("dependencies/first-order-model/config/vox-256.yaml",
+                                                         "checkpoints/vox-cpk.pth.tar",
+                                                         cpu=False)
+    @staticmethod
+    def generate_deepfake(image_path, driver_path, output_path="output/output.mp4"):
+        if not hasattr(DeepFakeApi, "initialized"):
+            DeepFakeApi.__init__()
 
-source_image = resize(source_image, (256, 256))[..., :3]
-driving_video = [resize(frame, (256, 256))[..., :3] for frame in driving_video]
-generator, kp_detector = load_checkpoints("dependencies/first-order-model/config/vox-256.yaml",
-                                          "C:/Users/Matis/Desktop/deepfake api/checkpoints/vox-cpk.pth.tar",
-                                          cpu=False)
+        print("Loading driver and source image")
+        source_image = imageio.imread(image_path)
+        reader = imageio.get_reader(driver_path)
 
-predictions = make_animation(source_image, driving_video, generator, kp_detector, cpu=False)
+        fps = reader.get_meta_data()['fps']
+        driving_video = []
+        try:
+            for im in reader:
+                driving_video.append(im)
+        except RuntimeError:
+            pass
+        reader.close()
 
-#save resulting video
-imageio.mimsave('output/output.mp4', [img_as_ubyte(frame) for frame in predictions], fps=fps)
+        print("Resizing driver and source image")
+        source_image = resize(source_image, (256, 256))[..., :3]
+        driving_video = [resize(frame, (256, 256))[..., :3] for frame in driving_video]
+
+        print("Creating animation")
+        predictions = make_animation(source_image, driving_video, DeepFakeApi.generator, DeepFakeApi.kp_detector, cpu=False)
+
+        # save resulting video
+        print("Saving output")
+        imageio.mimsave(output_path, [img_as_ubyte(frame) for frame in predictions], fps=fps)
+
+
+
+if __name__ == "__main__":
+    DeepFakeApi.generate_deepfake("input/obama.jpg", "input/baka mitai driver.mp4", output_path="output/output.mp4")
+
+
